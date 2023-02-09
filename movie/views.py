222394c452671c0a14d8c5f8bd4d17aece98330a
smaring2 <sebasmaringallego@hotmail.com>
2023-02-09 12:07:11 -0500

    archivo html

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Sebastián'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to about Page</h1>')
